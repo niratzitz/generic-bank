e8bf6bd037574cb483c45376032040d811bc869f
Effi <effoeffi@gmail.com>
2017-07-05 11:52:52 +0300

    dockerignore; added http server;

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+func main() {
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}).Methods(http.MethodGet)
+	go func() {
+		log.Info("Bank of America Server listening on port 8085")
+		if err := http.ListenAndServe(":8085", router); err != nil {
+			log.Error("Bank of America Server Interapted. ", err)
+		}
+	}()
+
+	<-stop // wait for SIGINT
+	log.Info("Bank of America Server has been stopped")
+}
