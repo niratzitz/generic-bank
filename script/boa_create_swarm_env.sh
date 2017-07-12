627037f84fd3adc05ef17c0d27608c100e7ee4c6
Effi <effoeffi@gmail.com>
2017-07-12 23:36:40 +0300

    create postgres swarm service;

--- script/boa_create_swarm_env.sh
+++ script/boa_create_swarm_env.sh
@@ -6,5 +6,8 @@ sudo docker network inspect boanet
 # redis
 sudo docker service create --replicas 1 --name redis-boa -p 6379:6379 --network boanet redis:3.2-alpine
 
+# postgres
+sudo service create --replicas 1 --name postgres-boa -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=123 -p 5432:5432 --network boanet postgres
+
 # bank of america
 sudo docker service create --replicas 1 --name bank-of-america -p 8085:8085 --network boanet tufinim/bank-of-america
