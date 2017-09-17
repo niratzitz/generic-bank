1ff56c8497a93665a23cba825d0a34c808d7a751
Effi <effoeffi@gmail.com>
2017-09-18 00:08:51 +0300

    added CD to admin on GKE;

---
+++ script/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker run --rm -v "$PWD":/work -w /work -e GCLOUD_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} tufinim/gcloud-sdk /script/kube_update_deployment_image.sh ${GCLOUD_PROJECT_ID} ${GCLOUD_CLUSTER_NAME} ${GCLOUD_ZONE1} deployment/admin admin=${IMAGE_NAME}:${CIRCLE_BUILD_NUM}
+if [ $? -ne 0 ];
+then
+    echo "Failed to update image on google cloud"
+fi
+
+exit 0 # build would not fail if deployment failed
