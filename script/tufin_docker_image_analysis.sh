ce797f37bc99a35710f43796cf72f230c95a67a9
Effi <effoeffi@users.noreply.github.com>
2017-09-18 13:55:12 +0300

    fix 401 asking for tufin bash script;

--- script/tufin_docker_image_analysis.sh
+++ script/tufin_docker_image_analysis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ok=`curl -I http://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+ok=`curl -X GET -I http://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 if [ "$ok" -ne 200 ]; then
     echo "Failed to get tufin docker analysis script with $ok"
     exit 0  # do not cause build failure
