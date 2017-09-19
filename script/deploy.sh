62442645ecca81cda483b84bd5569ccc5ccd9eaf
Effi <effoeffi@users.noreply.github.com>
2017-09-19 15:52:55 +0300

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${GOOGLE_APPLICATION_CREDENTIALS}
 docker run --rm -v "$PWD":/work -w /work -e GCLOUD_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} tufinim/gcloud-sdk /script/kube_update_deployment_image.sh ${GCLOUD_PROJECT_ID} ${GCLOUD_CLUSTER_NAME} ${GCLOUD_ZONE1} deployment/admin admin=${IMAGE_NAME}:${CIRCLE_BUILD_NUM}
 if [ $? -ne 0 ];
 then
