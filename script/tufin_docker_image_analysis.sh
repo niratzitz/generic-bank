056dae9bbb3af586f063cf9e25bb25d945e0e549
zvikaga <zvikaga@gmail.com>
2017-10-17 16:24:49 +0300

    Adding tufinim cert download and instructing curl to trust it;

--- script/tufin_docker_image_analysis.sh
+++ script/tufin_docker_image_analysis.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-ok=`curl -X GET -I http://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+# Downloading tufinim certificate...
+openssl s_client -showcerts -connect tufinim.hopto.org:443 < /dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > tufinim.pem
+
+# Downloading tufinim image scanning script
+ok=`curl -X GET -I -cacert tufinim.pem https://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 if [ "$ok" -ne 200 ]; then
     echo "Failed to get tufin docker analysis script with $ok"
     exit 0  # do not cause build failure
 fi
-bash <(curl -s http://tufinim.hopto.org/bash) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
+bash <(curl -s -cacert tufinim.pem https://tufinim.hopto.org/bash) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
