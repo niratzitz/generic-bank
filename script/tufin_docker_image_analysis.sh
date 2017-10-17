6d136e5498d3f58bfe2203828f2a5bdb581f7294
zvikaga <zvikaga@gmail.com>
2017-10-17 23:44:23 +0300

    revert to -k;

--- script/tufin_docker_image_analysis.sh
+++ script/tufin_docker_image_analysis.sh
@@ -4,7 +4,7 @@
 openssl s_client -showcerts -connect tufinim.hopto.org:443 < /dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > tufinim.pem
 
 # Downloading tufinim image scanning script
-ok=`curl -X GET -k -I https://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+ok=`curl -k -X GET -I https://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 if [ "$ok" -ne 200 ]; then
     echo "Failed to get tufin docker analysis script with $ok"
     exit 0  # do not cause build failure
