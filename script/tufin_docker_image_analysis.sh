287afa0885315c3f33ae1aeaa249bec74b9af6f6
zvikaga <zvikaga@gmail.com>
2017-10-17 17:04:18 +0300

    Fixing -cacert to --cacert;

--- script/tufin_docker_image_analysis.sh
+++ script/tufin_docker_image_analysis.sh
@@ -4,9 +4,9 @@
 openssl s_client -showcerts -connect tufinim.hopto.org:443 < /dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > tufinim.pem
 
 # Downloading tufinim image scanning script
-ok=`curl -X GET -I -cacert tufinim.pem https://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+ok=`curl -X GET -I --cacert tufinim.pem https://tufinim.hopto.org/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 if [ "$ok" -ne 200 ]; then
     echo "Failed to get tufin docker analysis script with $ok"
     exit 0  # do not cause build failure
 fi
-bash <(curl -s -cacert tufinim.pem https://tufinim.hopto.org/bash) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
+bash <(curl -s --cacert tufinim.pem https://tufinim.hopto.org/bash) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
