97ff70e76107ee913a79aa5c44274f517e5ca7f3
Zvika <zvikaga@gmail.com>
2018-03-08 16:04:21 +0200

    remove ssl ignore

--- script/tufin_docker_image_analysis.sh
+++ script/tufin_docker_image_analysis.sh
@@ -4,9 +4,10 @@
 #openssl s_client -showcerts -connect tufinim.hopto.org:443 < /dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > tufinim.pem
 
 # Downloading tufinim image scanning script
-ok=`curl -k -X GET -I https://api.tufin.io/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
-if [ "$ok" -ne 200 ]; then
+ok=`curl -X GET -I https://tufin.io/cia/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+if [ "$ok" -ne 200 ]
+then
     echo "Failed to get tufin docker analysis script with $ok"
     exit 0  # do not cause build failure
 fi
-bash <(curl -k -s https://api.tufin.io/bash) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
+bash <(curl -s https://tufin.io/cia/bash) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
