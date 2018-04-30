daa112490fe010b3f9768865001f24b694b82ac2
Effi <effoeffi@users.noreply.github.com>
2018-04-30 03:04:15 +0300

    Authorization header added;

--- script/tufin_docker_image_analysis.sh
+++ script/tufin_docker_image_analysis.sh
@@ -2,10 +2,10 @@
 
 # Downloading tufin image scanning script
 tufin_scan_url=https://${TUFIN_HOST}/api/scripts/image-scan
-ok=`curl -X GET -I $tufin_scan_url 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+ok=`curl -H "Authorization: Bearer $API_KEY" -X GET -I $tufin_scan_url 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 if [ "$ok" -ne 200 ]
 then
     echo "Failed to get tufin docker analysis script with $ok"
     exit 0  # do not cause build failure
 fi
-bash <(curl -s $tufin_scan_url) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
+bash <(curl -H "Authorization: Bearer $API_KEY" -s $tufin_scan_url) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
