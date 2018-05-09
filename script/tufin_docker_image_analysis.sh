b9e907c05cb06029b4b9af3bc01d957c4b8b820a
Effi <effoeffi@users.noreply.github.com>
2018-05-10 00:00:52 +0300

    api key added; [skip ci]

--- script/tufin_docker_image_analysis.sh
+++ script/tufin_docker_image_analysis.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# Downloading tufin certificate...
-#openssl s_client -showcerts -connect tufinim.hopto.org:443 < /dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > tufinim.pem
-
 # Downloading tufin image scanning script
-ok=`curl -X GET -I https://${TUFIN_HOST}/cia/bash 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+tufin_scan_url=https://${TUFIN_HOST}/api/scripts/image-scan
+ok=`curl -H "Authorization: Bearer $WD_TUFIN_API_KEY" -X GET -I $tufin_scan_url 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 if [ "$ok" -ne 200 ]
 then
     echo "Failed to get tufin docker analysis script with $ok"
     exit 0  # do not cause build failure
 fi
-bash <(curl -s https://${TUFIN_HOST}/cia/bash) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
+bash <(curl -H "Authorization: Bearer $WD_TUFIN_API_KEY" -s $tufin_scan_url) "$IMAGE_NAME:$CIRCLE_BUILD_NUM"
