9af55121b4b6e4b32c1f75f9a53f7140a7a862f4
Ohad Ben Nun <ohad.ben-nun@tufin.com>
2018-06-13 21:37:05 +0300

    debug angular app

--- ng-app/generic-bank/src/app/components/home/home.component.ts
+++ ng-app/generic-bank/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,7 @@ import {Component, ElementRef, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public elem: ElementRef) { }
+  constructor() { }
 
   ngOnInit() {
   }
