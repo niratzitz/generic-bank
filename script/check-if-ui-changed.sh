3a08bead43c678788ba50582a5826f5d298c066e
Ohad Ben Nun <ohad.ben-nun@tufin.com>
2018-06-24 19:04:22 +0300

    fix script

--- script/check-if-ui-changed.sh
+++ script/check-if-ui-changed.sh
@@ -1,10 +1,12 @@
 UI_COMMIT=$(git log -1 --format=format:%H --full-diff ng-app)
 LAST_COMMIT=$(git rev-parse HEAD)
 
+DIR=$(pwd)
+
 if [ $UI_COMMIT = $LAST_COMMIT ]; then
     echo "Changes to UI were made"
-    ./build-app.sh
+    $DIR/build-app.sh
 else
     echo "NONE"
-    ./fetch-release.sh
+    $DIR/fetch-release.sh
 fi
