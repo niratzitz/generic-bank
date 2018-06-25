7ded517887315caf762bc530fb7217da0ca35779
Ohad Ben Nun <ohad.ben-nun@tufin.com>
2018-06-25 14:02:25 +0300

    removed frontend project to a different repo, fetch UI from generic-bank-ui repo

--- ng-app/generic-bank/src/app/directives/async-progress.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AsyncProgressDirective } from './async-progress.directive';
-
-describe('AsyncProgressDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AsyncProgressDirective();
-    expect(directive).toBeTruthy();
-  });
-});
