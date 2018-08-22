8251b0fb415854d4b37ce0bcbd6a1d9daf3b5b7c
Effi <effoeffi@gmail.com>
2018-08-22 19:53:15 +0300

    update tomel & jenkins.sh;

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,5 @@
+#!/usr/bin/env bash
 set +x
-
-github_token=
-docker_user=
-docker_pass=
 
 image_name=tufinim/generic-bank
 gopath=/var/jenkins_home/tools/org.jenkinsci.plugins.golang.GolangInstallation/1.10.3
@@ -17,12 +14,12 @@ go build -o .dist/generic-bank
 go test
 
 # get ui
-curl -o ui.zip `curl -i -H "Accept: application/octet-stream" -H "Authorization: token $github_token" $(curl -s -H "Authorization: token $github_token" https://api.github.com/repos/Tufin/generic-bank-ui/releases/latest | grep url | grep releases\/assets | egrep -o "https://[^\"]+") | grep location: | cut -d " " -f2 | tr -d '\r\n'` && echo 'ignore cache'
+curl -o ui.zip `curl -i -H "Accept: application/octet-stream" -H "Authorization: token $GITHUB_TOKEN" $(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/Tufin/generic-bank-ui/releases/latest | grep url | grep releases\/assets | egrep -o "https://[^\"]+") | grep location: | cut -d " " -f2 | tr -d '\r\n'` && echo 'ignore cache'
 unzip ui.zip -d ./ui
 
 # create docker image and docker-hub deploy
 docker build -t $image_name .
 
-docker tag $image_name $image_name:$BUILD_NUMBER
-echo "$docker_pass" | docker login -u $docker_user --password-stdin
+docker tag $image_name $image_name:$BUILD_NUMBER$BUILD_NUMBER
+echo "$DOCKER_PASS" | docker login -u $docker_user --password-stdin
 docker push $image_name
