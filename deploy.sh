0108763aef37ed08653fe535e0e37be6a82559f9
shucks <reuvenharrison@gmail.com>
2018-12-31 17:13:18 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+main() {
+    TIMEZONEDB_API_KEY=${1:-$TIMEZONEDB_API_KEY}
+
+    [[ -z "$TIMEZONEDB_API_KEY" ]] && echo "'TIMEZONEDB_API_KEY' is missing!" && exit 1
+
+    sed -e 's/#TIMEZONEDB_API_KEY#/'"$TIMEZONEDB_API_KEY"'/g' \
+    aut/deployment/secrets/gbank-defaults.yaml > aut/deployment/secrets/gbank.yaml
+    kubectl create -f aut/deployment/secrets/gbank.yaml
+
+    local svc_type='NodePort'
+
+    # Redis
+    echo "Deploying Redis..."
+    kubectl create -f aut/deployment/db/aut_redis.yaml
+    kubectl create -f aut/deployment/db/aut_redis_svc.yaml
+
+    # PostgreSQL
+    echo "Deploying PostgreSQL..."
+    kubectl create -f aut/deployment/db/aut_postgres.yaml
+    kubectl create -f aut/deployment/db/aut_postgres_svc.yaml
+
+    # Customer
+    echo "Deploying Customers Service..."
+    kubectl create -f aut/deployment/customer/aut_customer.yaml
+
+    # Indexer
+    echo "Deploying Indexer..."
+    kubectl create -f aut/deployment/indexer/aut_indexer.yaml
+    kubectl create -f aut/deployment/indexer/aut_indexer_svc.yaml
+
+    # Time
+    echo "Deploying Time Service..."
+    kubectl create -f aut/deployment/time/aut_time.yaml
+    kubectl create -f aut/deployment/time/aut_time_svc.yaml
+
+    # Balance
+    echo "Deploying Balance Service..."
+    kubectl create -f aut/deployment/balance/aut_balance.yaml
+    kubectl create -f aut/deployment/balance/aut_balance_svc.yaml
+
+    # Admin
+    echo "Deploying Admin Service..."
+    kubectl create -f aut/deployment/admin/aut_admin.yaml
+
+    # Maintenance
+    echo "Deploying Maintenance Service..."
+    kubectl create -f aut/deployment/maintenance/aut_maintenance.yaml
+    kubectl create -f aut/deployment/maintenance/aut_maintenance_svc.yaml
+
+    # Ingress
+    kubectl create -f aut/deployment/ingress/ingress_gce.yaml
+
+    echo "Configuring service type for the exposed services..."
+    sed -e 's/#SVC_TYPE#/'"$svc_type"'/g' \
+    aut/deployment/admin/aut_admin_svc_template.yaml > aut/deployment/admin/aut_admin_svc.yaml
+    sed -e 's/#SVC_TYPE#/'"$svc_type"'/g' \
+    aut/deployment/customer/aut_customer_svc_template.yaml > aut/deployment/customer/aut_customer_svc.yaml
+
+    echo "Deploying admin Service..."
+    kubectl create -f aut/deployment/admin/aut_admin_svc.yaml
+    echo "Deploying customer Service..."
+    kubectl create -f aut/deployment/customer/aut_customer_svc.yaml
+}
+
+main $@
