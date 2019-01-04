e17e75d13f83e34efa4ba4ecf514810374f49ae3
shucks <reuvenharrison@gmail.com>
2019-01-04 14:41:44 +0200

    enable cluster create

--- create_aut_clusters.sh
+++ create_aut_clusters.sh
@@ -12,7 +12,7 @@ for (( c = $from; c < $to; c++ ))
 do
    name=`./random-word-generator.sh 1 | sed "s/'s//g"`
    echo "Creating cluster: "$name
-   #gcloud container clusters create $name --zone=europe-west1-b --num-nodes=1 --machine-type=n1-standard-2 --cluster-version=latest --preemptible --async 
+   gcloud container clusters create $name --zone=europe-west1-b --num-nodes=1 --machine-type=n1-standard-2 --cluster-version=latest --preemptible --async 
 done
 
 # for i in {1..20}; do gcloud container clusters get-credentials cluster$i; ./deploy.sh <key> ; done
