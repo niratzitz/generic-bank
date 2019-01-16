57485e4800faeb3e4b324d87c2b7341c80052569
shucks <reuvenharrison@gmail.com>
2019-01-16 11:00:27 +0200

    use a smaller subnet for pods

--- create_aut_clusters.sh
+++ create_aut_clusters.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 from to"
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 number-of-subnets"
     exit 1
 fi
 
-from=${1}
-to=${2}
+number=${1}
 
-for (( c = $from; c < $to; c++ ))
+for (( c = 0; c < $number; c++ ))
 do
    name=`./random-word-generator.sh 1 | sed "s/'s//g"`
    echo "Creating cluster: "$name
-   gcloud container clusters create $name --zone=europe-west1-b --num-nodes=1 --machine-type=n1-standard-2 --cluster-version=latest --preemptible --async 
+   gcloud container clusters create $name --zone=europe-west1-b --num-nodes=1 --machine-type=n1-standard-2 --cluster-version=latest --preemptible --async --cluster-ipv4-cidr=10.12.0.0/16
 done
 
 # for i in {1..20}; do gcloud container clusters get-credentials cluster$i; ./deploy.sh <key> ; done
