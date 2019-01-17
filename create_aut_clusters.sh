81c945ac00cd9639c5539f5630c67d88a1b10a13
shucks <reuvenharrison@gmail.com>
2019-01-17 12:29:03 +0200

    validate k8s cluster name

--- create_aut_clusters.sh
+++ create_aut_clusters.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+get_cluster_name() {
+    name=""
+    regex1="^[-a-z0-9]{1,40}$"
+    regex2="^[a-z].*[a-z0-9]$"
+    while ! [[ $name =~ $regex1 && $name =~ $regex2 ]]; do
+        name=`./random-word-generator.sh 1`
+    done
+}
+
 if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 number-of-subnets"
+    echo "Usage: $0 number-of-clusters"
     exit 1
 fi
 
 number=${1}
 
-for (( c = 0; c < $number; c++ ))
+for (( c = 0; c <= $number; c++ ))
 do
-   name=`./random-word-generator.sh 1 | sed "s/'s//g"`
+   get_cluster_name
    echo "Creating cluster: "$name
-   gcloud container clusters create $name --zone=europe-west1-b --num-nodes=1 --machine-type=n1-standard-2 --cluster-version=latest --preemptible --async --cluster-ipv4-cidr=10.12.0.0/16
+   gcloud container clusters create $name --zone=europe-west1-b --num-nodes=1 --machine-type=n1-standard-2 --cluster-version=latest --preemptible --async --cluster-ipv4-cidr=10.$c.0.0/16
 done
 
 # for i in {1..20}; do gcloud container clusters get-credentials cluster$i; ./deploy.sh <key> ; done
